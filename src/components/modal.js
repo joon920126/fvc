8dcd69ff74727753d3dbe3d3af66f28b9845c6e0
Joonhee Park <joon920126@gmail.com>
2021-10-13 13:29:16 +0900

    add modal, sample

---
+++ src/components/modal.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Modal = () => {
+    return ( 
+        <Wrapper>
+
+        </Wrapper>
+     );
+}
+ 
+export default Modal;
+
+const Wrapper = styled.div`
+
+`
+
+const Container = styled.div`
+
+`
---
+++ src/utils/sample.js
@@ -0,0 +1,26 @@
+const sample = [
+    {
+        isContacted: true,
+        date: '01.01.2021',
+        name: 'Anthony Affleck',
+        contact: '01012345678',
+        grow: true,
+        connect: true,
+        serve: false,
+        prayer: false,
+        referTo: 'James'
+    },
+    {
+        isContacted: false,
+        date: '02.02.2021',
+        name: 'Brad Bailey',
+        contact: '01087654321',
+        grow: false,
+        connect: false,
+        serve: true,
+        prayer: true,
+        referTo: 'Kerey'
+    },
+]
+
+export default sample
